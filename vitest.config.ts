77569c8256de2e47a2151a7d0909916a37c2a465
v-misho-tektumanidze <v.misho.tektumanidze@reddit.com>
2026-01-05 17:12:20 +0400

    Adds code coverage with min thresholds

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,16 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "jsdom",
+    coverage: {
+      thresholds: {
+        perFile: true,
+        "src/**/*.ts": {
+          lines: 95,
+          statements: 95,
+          functions: 100,
+          branches: 100,
+        },
+      },
+    },
   },
 });
