7a131460126b10db320de0ab77c72d262e265151
v-misho-tektumanidze <v.misho.tektumanidze@reddit.com>
2026-01-05 15:28:07 +0400

    Adds husky package

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,23 +1,25 @@
-import {beforeEach, describe, expect, test, vi} from 'vitest'
-import { onSubmit } from './index'
+import { beforeEach, describe, expect, test, vi } from "vitest";
+import { onSubmit } from "./index";
 
-describe('onSubmit', () => {
-
+describe("onSubmit", () => {
   let e: SubmitEvent;
-  let formEl: HTMLFormElement, tableEl: HTMLTableElement, categoryInputEl: HTMLInputElement, amountInputEl: HTMLInputElement;
+  let formEl: HTMLFormElement,
+    tableEl: HTMLTableElement,
+    categoryInputEl: HTMLInputElement,
+    amountInputEl: HTMLInputElement;
 
   beforeEach(() => {
-    e = new SubmitEvent('');
+    e = new SubmitEvent("");
 
-    formEl = document.createElement('form');
-    tableEl = document.createElement('table');
+    formEl = document.createElement("form");
+    tableEl = document.createElement("table");
 
-    categoryInputEl = document.createElement('input');
-    categoryInputEl.setAttribute('name', 'category');
+    categoryInputEl = document.createElement("input");
+    categoryInputEl.setAttribute("name", "category");
     formEl.appendChild(categoryInputEl);
 
-    amountInputEl = document.createElement('input');
-    amountInputEl.setAttribute('name', 'amount');
+    amountInputEl = document.createElement("input");
+    amountInputEl.setAttribute("name", "amount");
     formEl.appendChild(amountInputEl);
   });
 
@@ -30,30 +32,32 @@ describe('onSubmit', () => {
     onSubmit(e, formEl, tableEl);
   }
 
-  test('prevents default on the submit event', () => {
-    const preventDefaultSpy = vi.spyOn(e, 'preventDefault');
+  test("prevents default on the submit event", () => {
+    const preventDefaultSpy = vi.spyOn(e, "preventDefault");
     submit();
 
     expect(preventDefaultSpy).toHaveBeenCalledOnce();
   });
 
-  test('appends a new row to the table', () => {
-    setFormValues('Bills', 24);
+  test("appends a new row to the table", () => {
+    setFormValues("Bills", 24);
     submit();
 
-    const rows = tableEl.querySelectorAll('tr');
+    const rows = tableEl.querySelectorAll("tr");
     expect(rows).lengthOf(1);
 
-    const [categoryCellEl, amountCellEl] = Array.from(rows[0].querySelectorAll('td'));
-    expect(categoryCellEl.innerText).toBe('Bills');
-    expect(amountCellEl.innerText).toBe('24$');
+    const [categoryCellEl, amountCellEl] = Array.from(
+      rows[0].querySelectorAll("td"),
+    );
+    expect(categoryCellEl.innerText).toBe("Bills");
+    expect(amountCellEl.innerText).toBe("24$");
   });
 
-  test('resets the form values', () => {
-    setFormValues('Car repayment', 400);
+  test("resets the form values", () => {
+    setFormValues("Car repayment", 400);
     submit();
 
     expect(categoryInputEl.value).toBe("");
     expect(amountInputEl.value).toBe("");
-  })
+  });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-  root: resolve(__dirname, 'src'),
+  plugins: [tailwindcss()],
+  root: resolve(__dirname, "src"),
   build: {
-    outDir: resolve(__dirname, 'dist'),
-  }
-})
+    outDir: resolve(__dirname, "dist"),
+  },
+});
